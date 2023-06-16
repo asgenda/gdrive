0631c086dccac0dd6b6b351b821e7fa4379b89ac
asgenda <59675909+asgenda@users.noreply.github.com>
2023-06-16 15:34:26 -0400

    Added gdrive "hard links" to shared files.

--- genda-import.R
+++ genda-import.R
@@ -1,2 +1,11 @@
 #install.packages("googledrive")
-library("googledrive")
+library(googledrive)
+library(readr)
+drive_find(n_max =2)
+drive_download(as_id("11UCRwzRnYRsX54hgYCkyn0pcyZq_Hr0k"),overwrite=TRUE) #1000genomes.Rdata
+drive_download(as_id("1pyObSyY-aAxj1KLT3Hxi1kyaDolRngrk"),overwrite=TRUE) #1000genomes.r 
+drive_download(as_id("1UEdAKvzTgV5pyt8ARib3GRkwwd-AfG9x"),overwrite=TRUE) #1000genomes.pdf
+load("1000genomes.Rdata")
+
+drive_download(as_id("1oXdOYjUbz25H7NWJJX2Y-ypCPzxhHvQ2"),overwrite=TRUE) #brain_subset_metadata.csv
+brain_subset_metadata <- read_csv("brain_subset_metadata.csv")
